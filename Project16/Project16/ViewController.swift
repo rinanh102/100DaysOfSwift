5d6eb6cfc5ba72a410561525c377d3744c549cce
Henry Nguyen <nvqdung@gmail.com>
2019-04-19 12:58:13 -0400

    Part 1 of Project16

--- Project16/Project16/ViewController.swift
+++ Project16/Project16/ViewController.swift
@@ -46,7 +46,14 @@ class ViewController: UIViewController, MKMapViewDelegate {
     }
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-         
+        guard let capital = view.annotation as? Capital else { return }
+        let placeName = capital.title
+        let placeInfo = capital.info
+        
+        let alert = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        
+        present(alert, animated:  true)
     }
 
 
